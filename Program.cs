5ee0951a28b939820d3b69f5845ae90fda99f490
Thiago Ramos <thiago.s.ramos@outlook.com>
2024-02-28 20:33:22 -0300

    Implementado solucao da atividade calculo folha

--- Program.cs
+++ Program.cs
@@ -6,7 +6,31 @@ namespace CalculoFolha
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Dados funcionario
+            string nome;
+            double salario;
+            int imposto;
+
+            Console.WriteLine("Informe o nome do funcionario");
+            nome = Console.ReadLine();
+            Console.WriteLine("Informe o salario inical.");
+            salario = double.Parse(Console.ReadLine());
+            Console.WriteLine("Informe a taxa de imposto cobrado (%).");
+            imposto = int.Parse(Console.ReadLine());
+            double desconto = salario * imposto / 100;
+            double salarioLiquido = salario - desconto;
+            Console.WriteLine($"Nome: {nome}");
+            Console.WriteLine("Salario Liquido: " + salarioLiquido);
+            string sep = new string('=',50);
+            Console.WriteLine(sep);
+            Console.WriteLine("Informe qual a porcentagem do aumento");
+            int aumento = int.Parse(Console.ReadLine());
+            double promocao = salario * aumento / 100;
+            salario += promocao;
+            desconto = salario * imposto / 100;
+            salarioLiquido = salario - desconto;
+            Console.WriteLine("Salario bruto :" + salario);
+            Console.WriteLine("Salario liquido :" + salarioLiquido);
         }
     }
 }
