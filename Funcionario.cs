c91a00d9dbd039a6659d2fa5571717c0846a00f3
Thiago Ramos <thiago.s.ramos@outlook.com>
2024-02-28 22:03:05 -0300

    Criada sobreposicao do metodo ToString() e corrigido fluxo no main

--- Funcionario.cs
+++ Funcionario.cs
@@ -17,5 +17,13 @@ namespace CalculoFolha
             double valorAumento = Salario * aumento /100;
             Salario += valorAumento;
         }
+
+        public override string ToString()
+        {
+            string sep = new string('=', 50);
+            double salarioLiquido = CalculaSalarioLiquido();
+            string mensagem = $"{sep}\nNome: {Nome}\nSalario bruto: {Salario.ToString("C")}\nSalario liquido {salarioLiquido.ToString("C")}\n{sep}";
+            return mensagem;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,25 +7,18 @@ namespace CalculoFolha
         static void Main(string[] args)
         {
             //Dados funcionario
-            Funcionario funcionario = new Funcionario();  
-
+            Funcionario funcionario = new Funcionario();
             Console.WriteLine("Informe o nome do funcionario");
             funcionario.Nome = Console.ReadLine();
             Console.WriteLine("Informe o salario inical.");
             funcionario.Salario = double.Parse(Console.ReadLine());
             Console.WriteLine("Informe a taxa de imposto cobrado (%).");
-            funcionario.Imposto = int.Parse(Console.ReadLine());           
-            double salarioLiquido = funcionario.CalculaSalarioLiquido();
-            Console.WriteLine($"Nome: {funcionario.Nome}");
-            Console.WriteLine("Salario Liquido: " + salarioLiquido);
-            string sep = new string('=',50);
-            Console.WriteLine(sep);
+            funcionario.Imposto = int.Parse(Console.ReadLine());            
+            Console.WriteLine(funcionario);
             Console.WriteLine("Informe qual a porcentagem do aumento");
             int aumento = int.Parse(Console.ReadLine());
             funcionario.CalculaAumentoSalario(aumento);     
-            salarioLiquido = funcionario.CalculaSalarioLiquido();
-            Console.WriteLine("Salario bruto :" + funcionario.Salario);
-            Console.WriteLine("Salario liquido :" + salarioLiquido);
+            Console.WriteLine(funcionario.ToString());
         }
     }
 }
