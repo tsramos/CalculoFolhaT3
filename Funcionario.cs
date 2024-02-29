3b5f1858afefc0831e4518cb93623060211e728c
Thiago Ramos <thiago.s.ramos@outlook.com>
2024-02-28 21:29:53 -0300

    Adicionados metodos aumentasalario e calculaSalarioLiquido para classe Funcionario

--- Funcionario.cs
+++ Funcionario.cs
@@ -5,5 +5,17 @@ namespace CalculoFolha
         public string Nome;
         public double Salario;
         public int Imposto;
+
+        public double CalculaSalarioLiquido()
+        {
+            double desconto = Salario * Imposto / 100;
+            return Salario - desconto;
+        }
+
+        public void CalculaAumentoSalario(int aumento)
+        {
+            double valorAumento = Salario * aumento /100;
+            Salario += valorAumento;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -15,29 +15,17 @@ namespace CalculoFolha
             funcionario.Salario = double.Parse(Console.ReadLine());
             Console.WriteLine("Informe a taxa de imposto cobrado (%).");
             funcionario.Imposto = int.Parse(Console.ReadLine());           
-            double salarioLiquido = CalculaSalarioLiquido(funcionario.Imposto, funcionario.Salario);
+            double salarioLiquido = funcionario.CalculaSalarioLiquido();
             Console.WriteLine($"Nome: {funcionario.Nome}");
             Console.WriteLine("Salario Liquido: " + salarioLiquido);
             string sep = new string('=',50);
             Console.WriteLine(sep);
             Console.WriteLine("Informe qual a porcentagem do aumento");
             int aumento = int.Parse(Console.ReadLine());
-            funcionario.Salario = CalculaAumentoSalario(aumento, funcionario.Salario);      
-            salarioLiquido = CalculaSalarioLiquido(funcionario.Imposto, funcionario.Salario);
+            funcionario.CalculaAumentoSalario(aumento);     
+            salarioLiquido = funcionario.CalculaSalarioLiquido();
             Console.WriteLine("Salario bruto :" + funcionario.Salario);
             Console.WriteLine("Salario liquido :" + salarioLiquido);
-        }
-
-        public static double CalculaAumentoSalario(int taxa, double salario)
-        {
-            double aumento = salario * taxa / 100;
-            return salario + aumento;
-        }
-
-        public static double CalculaSalarioLiquido(int imposto , double salario)
-        {
-            double desconto = salario * imposto / 100;
-            return salario - desconto;
         }
     }
 }
